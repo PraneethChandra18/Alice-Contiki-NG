8084e32fa8b8229ad60d3826068bb57e15fbf808
PraneethChandra18 <52711274+PraneethChandra18@users.noreply.github.com>
2021-11-10 22:32:51 +0530

    Alice implementation with analysis

---
+++ examples/6tisch/simple-node-ksh/node.c
@@ -0,0 +1,295 @@
+/*
+ * Copyright (c) 2015, SICS Swedish ICT.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ */
+/**
+ * \file
+ *         A RPL+TSCH node able to act as either a simple node (6ln),
+ *         DAG Root (6dr) or DAG Root with security (6dr-sec)
+ *         Press use button at startup to configure.
+ *
+ * \author Simon Duquennoy <simonduq@sics.se>
+ */
+
+#include "contiki.h"
+#include "sys/node-id.h"
+#include "sys/log.h"
+#include "net/ipv6/uip-ds6-route.h"
+#include "net/ipv6/uip-sr.h"
+#include "net/mac/tsch/tsch.h"
+#include "net/routing/routing.h"
+
+
+#include "lib/random.h"
+#include "sys/ctimer.h"
+#include "net/ipv6/uip-ds6.h"
+#include "net/ipv6/uip-udp-packet.h"
+
+
+#define DEBUG DEBUG_PRINT
+#include "net/ipv6/uip-debug.h"
+
+/*---------------------------------------------------------------------------*/
+PROCESS(node_process, "RPL Node");
+AUTOSTART_PROCESSES(&node_process);
+
+
+static struct uip_udp_conn *server_conn;
+static struct uip_udp_conn *client_conn;
+static uip_ipaddr_t server_ipaddr;
+
+
+
+static int is_coordinator;
+int sent=0;
+int received=0;
+
+
+
+
+
+/*---------------------------------------------------------------------------*/
+
+static void
+send_packet(void *ptr)
+{
+  char buf[100];
+
+  sent++;
+  // PRINTF("DATA send to %d 'Hello I am %d'\n", server_ipaddr.u8[sizeof(server_ipaddr.u8) - 1], node_id);
+  PRINTF("DATA send to %d 'Hello I am %d'\n", 1, node_id);
+  sprintf(buf, "Hello I am %d", node_id);
+  
+  if(NETSTACK_ROUTING.get_root_ipaddr(&server_ipaddr)) {
+    //send packet through client_conn to UDP server
+    uip_udp_packet_sendto(client_conn, buf, strlen(buf), &server_ipaddr, UIP_HTONS(UDP_SERVER_PORT));
+  }
+  
+ // sent = sent+1;
+//  PRINTF("send_packet() %d\n", sent);
+
+
+
+//  rpl_instance_t *instance =rpl_get_default_instance();
+//  uip_ip6addr(&server_ipaddr, UIP_DS6_DEFAULT_PREFIX, 0, 0, 0, 0, 0x00ff, 0xfe00, 1);
+
+
+
+  // if(NETSTACK_ROUTING.get_root_ipaddr(&server_ipaddr)){
+
+  // printf("client-server addr : "); PRINT6ADDR(&server_ipaddr);
+  
+  // PRINTF("Message transmitted \n");
+
+  // sprintf(buf, "x");
+  // uip_udp_packet_sendto(client_conn, buf, strlen(buf), &server_ipaddr, UIP_HTONS(UDP_SERVER_PORT));
+
+  // }
+
+
+}
+
+/*---------------------------------------------------------------------------*/
+
+static void
+tcpip_handler(void)
+{
+  char *appdata;
+
+  if(uip_newdata()) {
+    appdata = (char *)uip_appdata;
+    appdata[uip_datalen()] = 0;
+    //print the data of packet
+    PRINTF("DATA recv from %d '%s'", UIP_IP_BUF->srcipaddr.u8[sizeof(UIP_IP_BUF->srcipaddr.u8) - 1], appdata);
+    PRINTF("\n");
+    
+    if(is_coordinator) {
+    
+      PRINTF("DATA send to %d 'ACK'\n", UIP_IP_BUF->srcipaddr.u8[sizeof(UIP_IP_BUF->srcipaddr.u8) - 1]);
+      
+      char buf[25];
+      sprintf(buf, "DATA send ACK");
+
+      uip_ipaddr_copy(&server_conn->ripaddr, &UIP_IP_BUF->srcipaddr);// &UIP_IP_BUF->srcipaddr);
+      uip_udp_packet_send(server_conn, buf, strlen(buf));
+      uip_create_unspecified(&server_conn->ripaddr);
+    }
+  }
+  
+  // received = received+1;
+//  printf("received_packet() %d\n", received);
+
+
+  // if(is_coordinator && uip_newdata()){ //server reply
+
+  //  send_packet(&UIP_IP_BUF->srcipaddr);
+
+
+    // char buf[10];
+    // sprintf(buf, "x");
+
+    // uip_ipaddr_copy(&server_conn->ripaddr, &UIP_IP_BUF->srcipaddr);// &UIP_IP_BUF->srcipaddr);
+    // uip_udp_packet_send(server_conn, buf, strlen(buf));
+    // uip_create_unspecified(&server_conn->ripaddr);
+
+    // sent = sent+1;
+//    PRINTF("send_packet() %d\n", sent);
+
+
+
+  //}
+}
+
+/*---------------------------------------------------------------------------*/
+
+static void
+set_global_address(void)
+{
+/*
+  uip_ipaddr_t ipaddr;
+  uip_ip6addr(&ipaddr, UIP_DS6_DEFAULT_PREFIX, 0, 0, 0, 0, 0, 0, 0);
+  uip_ds6_set_addr_iid(&ipaddr, &uip_lladdr);
+  uip_ds6_addr_add(&ipaddr, 0, ADDR_AUTOCONF);
+  printf("my addr : "); PRINT6ADDR(&ipaddr);
+
+*/
+//  uip_ip6addr(&server_ipaddr, UIP_DS6_DEFAULT_PREFIX, 0, 0, 0, 0, 0, 0, 1);
+//  uip_ip6addr(&server_ipaddr, 254, 254, 254, 0, 1, 1, 1, 1);
+//  printf("server addr : "); PRINT6ADDR(&server_ipaddr);
+}
+
+
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(node_process, ev, data)
+{
+
+
+
+  PROCESS_BEGIN();
+
+  is_coordinator = 0;
+
+#if CONTIKI_TARGET_COOJA || CONTIKI_TARGET_Z1
+  is_coordinator = (node_id == 1);
+#endif
+
+
+
+if(!is_coordinator){
+
+  set_global_address();
+
+  client_conn = udp_new(NULL, UIP_HTONS(UDP_SERVER_PORT), NULL); 
+  if(client_conn == NULL) {
+    PRINTF("No UDP connection available, exiting the process!\n");
+    PROCESS_EXIT();
+  }
+  udp_bind(client_conn, UIP_HTONS(UDP_CLIENT_PORT)); 
+
+  PRINTF("Created a connection with the server ");
+  PRINT6ADDR(&client_conn->ripaddr);
+  PRINTF(" local/remote port %u/%u\n",
+	UIP_HTONS(client_conn->lport), UIP_HTONS(client_conn->rport));
+}else{
+
+
+  uip_ipaddr_t ipaddr;
+  uip_ip6addr(&ipaddr, UIP_DS6_DEFAULT_PREFIX, 0, 0, 0, 0, 0, 0, 1);
+  uip_ds6_addr_add(&ipaddr, 0, ADDR_AUTOCONF);
+
+  PRINTF("SERVER ADDR: ");
+  PRINT6ADDR(&ipaddr);
+  PRINTF("\n");
+
+  server_conn = udp_new(NULL, UIP_HTONS(UDP_CLIENT_PORT), NULL);
+  if(server_conn == NULL) {
+    PRINTF("No UDP connection available, exiting the process!\n");
+    PROCESS_EXIT();
+  }
+  udp_bind(server_conn, UIP_HTONS(UDP_SERVER_PORT));
+
+  PRINTF("Created a server connection with remote address ");
+  PRINT6ADDR(&server_conn->ripaddr);
+  PRINTF(" local/remote port %u/%u\n", UIP_HTONS(server_conn->lport),
+         UIP_HTONS(server_conn->rport));
+
+
+}
+
+
+
+
+  if(is_coordinator) {
+    NETSTACK_ROUTING.root_start();
+  }
+  NETSTACK_MAC.on();
+
+
+
+#if WITH_PERIODIC_ROUTES_PRINT
+  {
+    static struct etimer et;
+    static struct ctimer backoff_timer;
+    /* Print out routing tables every minute */
+    etimer_set(&et, SEND_INTERVAL);
+
+
+
+  while(1) {
+    PROCESS_YIELD();
+    if(ev == tcpip_event) {
+      tcpip_handler();
+    }
+
+    if(etimer_expired(&et)) {
+      
+      #if (UIP_MAX_ROUTES != 0)
+        PRINTF("Routing entries: %u\n", uip_ds6_route_num_routes());
+      #endif
+      #if (UIP_SR_LINK_NUM != 0)
+        PRINTF("Routing links: %u\n", uip_sr_num_nodes());
+      #endif
+      
+      PRINTF("srsr %d/%d\n", sent, received);
+
+      if(!is_coordinator){
+        ctimer_set(&backoff_timer, SEND_TIME, send_packet, NULL);
+      }
+      etimer_restart(&et);
+
+    }
+  }
+
+
+
+  }
+#endif /* WITH_PERIODIC_ROUTES_PRINT */
+
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
